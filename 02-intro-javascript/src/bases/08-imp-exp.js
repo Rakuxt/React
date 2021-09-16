60e47004f3036e4ff22f1245728f4d52f2ae7467
Rakuxt <andres91_07@hotmail.com>
2021-09-15 23:12:51 -0500

    Promesas

--- 02-intro-javascript/src/bases/08-imp-exp.js
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -1,9 +1,11 @@
 // import { heroes } from './data/heroes';
 // import { heroes } from './data/heroes'
 // import {heroes, owners } from './data/heroes'
-import heroes, { owners } from '../data/heroes'
+// import heroes, { owners } from '../data/heroes'
+import heroes from '../data/heroes'
 
-console.log(owners);
+
+// console.log(owners);
 
 // const getHeroeById = (id) => {
 //     return heroes.find( ( heroe ) => {
@@ -14,11 +16,11 @@ console.log(owners);
 // })};
 
 
-const getHeroeById = (id) => heroes.find( heroe => heroe.id === id);
+export const getHeroeById = (id) => heroes.find( heroe => heroe.id === id);
 
-console.log( getHeroeById(2) );
+// console.log( getHeroeById(2) );
 
-const getHeroesByOwner = ( owner ) => heroes.filter( heroe => heroe.owner === owner);
+export const getHeroesByOwner = ( owner ) => heroes.filter( heroe => heroe.owner === owner);
 
-console.log( getHeroesByOwner('Marvel') );
+// console.log( getHeroesByOwner('Marvel') );
 
